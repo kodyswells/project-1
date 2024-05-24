18ef5f3fc5136cf559a2c5209fcae672deab7ea0
Kody Wells <kodyswells@gmail.com>
2024-05-24 13:26:07 -0600

    solved part-2

--- part-2/part_2.py
+++ part-2/part_2.py
@@ -1,29 +1,33 @@
 ### Step 1 - Lists ###
 
 # Fill in this list with several authors you are a fan of. At least 7 or 8 should do.
-my_authors = []
-
+my_authors = ["Robert Jordan", "Brandon Sanderson", "Orson Scott Card" ,"Rick Riodrin", "C.S Lewis" ,"Suzanne Collins" ,"Taran Matharu"]
+print(my_authors)
 # Now let's add a new author to the end with the .append() method. Type your code below.
 
-# Code here
+my_authors.append("J.K Rowling")
+print(my_authors)
 # Example: my_authors.append("H.G. Wells")
 
 
 # Now let's remove an element in the list
 
-# Code here
+my_authors.remove("Suzanne Collins")
+print(my_authors)
 # Example: my_authors.remove("H.G. Wells")
 
 
 # Now access an element by it's index. (Remember it indexes start at 0.)
 
-# Code here
+my_authors[0]
+print(my_authors[0])
 # Example: my_authors[2]
 
 
 # Now slice the list.
 
-# Code here
+my_authors[0:3]
+print(my_authors[0:3])
 # Example: my_authors[1:4]
 
 
@@ -31,7 +35,7 @@ my_authors = []
 
 # Create your tuple below. Assign it to a variable name you can reference later in the exercise.
 
-# Code here
+my_author_tuple = ("Robert Jordan", "Brandon Sanderson", "Orson Scott Card" ,"Rick Riodrin", "C.S Lewis" ,"Suzanne Collins" ,"Taran Matharu")
 # Example: my_author_tuple = ("F. Scott Fitzgerald", "J.K. Rowling", "Ernest Hemingway", "Emily Dickenson", "George Orwell", "Ray Bradbury")
 
 
@@ -39,19 +43,20 @@ my_authors = []
 
 # Create a set with your authors.
 
-# Code here
+my_author_set = {"Robert Jordan", "Brandon Sanderson", "Orson Scott Card" ,"Rick Riodrin", "C.S Lewis" ,"Suzanne Collins" ,"Taran Matharu"}
 # Example: my_author_set = {"F. Scott Fitzgerald", "J.K. Rowling", "Ernest Hemingway", "Emily Dickenson", "George Orwell", "Ray Bradbury"}
 
 
 # Add a new author to your set.
 
-# Code here
+my_author_set.add("Ray Bradbury")
 # Example: my_author_set.add("J.R.R. Tolkien")
-
+print(my_author_set)
 
 # Try adding the same author again, and be sure to print your set.
 
-# Code here
+my_author_set.add("Ray Bradbury")
+print(my_author_set)
 # Example: my_author_set.add("J.R.R. Tolkien")
 
 
@@ -60,7 +65,14 @@ my_authors = []
 
 # Create a for-loop for each of the data-structures above.
 
-# Code here
+for book in my_authors:
+    print(book)
+
+for book in my_author_tuple:
+    print(book)
+
+for book in my_author_set:
+    print(book)
 # Example:
 
 # for book in my_authors:
