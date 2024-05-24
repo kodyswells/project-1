64f237cbcabdc11abaad057c3aa0bb2d7abeef97
Kody Wells <kodyswells@gmail.com>
2024-05-24 11:24:15 -0600

    finished part 1

--- part-1/part_1.py
+++ part-1/part_1.py
@@ -3,7 +3,7 @@
 # Step 2 - Strings
 # Let's start with Python strings. Replace the "None" with the name of a book, and below the name of the author of your book.
 
-book_name = "Wheel of Time"
+book_name = "Eye of the World"
 author = "Robert Jordan"
 # book_name = "Dune"
 # author = "Frank Herbert"
@@ -20,22 +20,31 @@ sentence2 = f"Author, {author}, wrote the book {book_name}."
 
 # Step 4 - Integers
 # Now let's practice Integers. Replace the "None" below with the publication date of your book.
-publication_year = None
+publication_year = 1990
 # publication_year = 1965
 
 
 # Step 5 - Floats
 # Now estimate what the retail value of your book would be at a store and replace the "None" below with a float value of the price. (Doesn't have to be accurate.)
-book_price = None
+book_price = 35.99
 # book_price = 17.99
 
 
 # Step 6 - Booleans
 # Now we will practice with booleans. Replace the "is_awesome" variable with a boolean. True if your book is awesome, False if your book is not awesome.
-is_awesome = None
+is_awesome = True
 # is_awesome = True
 
 
 # Step 7 - print and type functions
 # Follow the instructions, and below this line, code all of the suggested print statements and the type statements.
-# Code below
+
+print(book_name)
+print(author)
+print(sentence1)
+print(sentence2)
+print(publication_year)
+print(book_price)
+print(is_awesome)
+
+print(type(book_name))
